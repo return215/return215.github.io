637b73c40cdc73ec0750ee45e7ff72c25ec9e06a
return215 <51498719+return215@users.noreply.github.com>
2024-01-05 09:45:28 +0700

    🧹 Set up linting and formatting
    Also cleaned up the code right after

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+/** @type {import('prettier').Config} */
+export default {
+    arrowParens: "always",
+    semi: false,
+    singleQuote: false,
+    trailingComma: "all",
+    printWidth: 80,
+    experimentalTernaries: true,
+    tabWidth: 4,
+    useTabs: false,
+    quoteProps: "as-needed",
+    bracketSpacing: true,
+    bracketSameLine: false,
+    endOfLine: "lf",
+    embeddedLanguageFormatting: "auto",
+    singleAttributePerLine: true,
+}
